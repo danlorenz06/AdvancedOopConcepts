35a46d8d7529896b8951b0f31353ab28c0f37293
Dan Lorenz Altaras <danlorenz06>
2021-04-26 22:13:46 +0800

    first commit

---
+++ Book.java
@@ -0,0 +1,27 @@
+package com.books;
+
+public class Book {
+	// Declaring variables
+	private String title;
+	protected double price;
+
+	// Parameterized constructor
+	public Book(String title) {
+		super();
+		this.title = title;
+	}
+
+	// Getter methods
+	public String getTitle() {
+		return title;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	// Abstract method
+	public void setPrice() {
+	}
+
+}
